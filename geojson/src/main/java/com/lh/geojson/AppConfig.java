a32424f4adbecd63ca75c76a071e124cc0785917
lh <beyondlh@126.com>
2016-11-17 09:19:19 +0800

    添加多个属性配置文件

--- geojson/src/main/java/com/lh/geojson/AppConfig.java
+++ geojson/src/main/java/com/lh/geojson/AppConfig.java
@@ -1,25 +1,21 @@
 package com.lh.geojson;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import org.geotools.data.*;
+import ch.qos.logback.classic.Level;
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.Query;
 import org.geotools.feature.FeatureCollection;
-import org.geotools.feature.FeatureIterator;
 import org.geotools.geojson.feature.FeatureJSON;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.filter.Filter;
-import org.opengis.filter.FilterFactory2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.support.StaticListableBeanFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,22 +27,52 @@ import java.util.Map;
  */
 @Component
 public class AppConfig {
-
+    /*1.logback首先会试着查找logback.groovy文件;
+    2.当没有找到时，继续试着查找logback-test.xml文件;
+    3.当没有找到时，继续试着查找logback.xml文件;
+    4.如果仍然没有找到，则使用默认配置（打印到控制台）。
+    TRACE < DEBUG < INFO < WARN < ERROR
+    */
     Logger logger = LoggerFactory.getLogger(AppConfig.class);
 
-    @Value("${shape.dir}")
+//    @Value("${shape.dir}")
+    @Value("${mytest.value}")
     String filePath;
 
     @PostConstruct
-    void readShapes() throws IOException {
+    void getFeaturesFromShape() throws IOException {
+        logger.info("logger.getName(),{}", logger.getName());
+//        ch.qos.logback.classic.Logger logger =
+//                (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("com.foo");
+//        logger.setLevel(Level. INFO);
 
-        logger.info("test");
+        logger.trace("======trace");
+        if(logger.isDebugEnabled()){
+            logger.debug("======debug");
+        }
+        logger.info("======info");
+        logger.warn("======warn");
+        logger.error("======error");
 
-        File file = new File(this.filePath);
+/*
+        String name = "Aub";
+        String message = "3Q";
+        String[] fruits = {"apple", "banana"};
+
+        // logback提供的可以使用变量的打印方式，结果为"Hello,Aub!"
+        logger.info("Hello,{}!", name);
+
+        // 可以有多个参数,结果为“Hello,Aub! 3Q!”
+        logger.info("Hello,{}!   {}!", name, message);
+
+        // 可以传入一个数组，结果为"Fruit:  apple,banana"
+        logger.info("Fruit:  {},{}", fruits);*/
+
+
+        /*File file = new File(this.filePath);
         Map<String, Object> map = new HashMap<>();
         map.put("url", file.toURI().toURL());
         map.put("charset", "gbk");
-//        map.put("charset", "UTF-8");
 
         DataStore dataStore = DataStoreFinder.getDataStore(map);
         FeatureSource<SimpleFeatureType, SimpleFeature> source = null;
@@ -66,9 +92,6 @@ public class AppConfig {
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         Query qurey = new Query();
         qurey.setMaxFeatures(1000);
-//        qurey.setFilter();
-        int i = 0;
-        int off = 0;
         long start = System.currentTimeMillis();
         for (String typeName : typeNames) {
             StringWriter writer = new StringWriter();
@@ -76,53 +99,13 @@ public class AppConfig {
             source = dataStore.getFeatureSource(typeName);
             collection = source.getFeatures(qurey);
             fjson.writeFeatureCollection(collection, writer);
-
             String json = writer.toString();
-//
             fileWriter.flush();
             fileWriter.write(json);
 
-
-//            bufferedWriter.flush();
-//            bufferedWriter.write(json);
         }
         fileWriter.close();
-//        bufferedWriter.close();
-        FilterFactory2 ff;
-
-
         long end = System.currentTimeMillis();
-        System.out.println(end - start + "ms");
-        /*int i = 0;
-        try (FeatureIterator<SimpleFeature> features = collection.features()) {
-            while (features.hasNext()) {
-                i++;
-                if (i > 10) {
-                    return;
-                }
-                SimpleFeature feature = features.next();
-                System.out.println(feature.toString());
-                System.out.println(JSON.toJSONString(feature.getDefaultGeometry().toString(), SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullBooleanAsFalse
-                ));
-            }
-        }*/
-
-//        System.out.print(json);
-//        int i = 0;
-//        try (FeatureIterator<SimpleFeature> features = collection.features()) {
-//            while (features.hasNext()) {
-//                i++;
-//                if (i > 10) {
-//                    return;
-//                }
-//                SimpleFeature feature = features.next();
-//                simpleFeatureList.add(feature);
-//                fjson.writeFeature(feature, writer);
-//                fjson.writeFeatureCollection(collection,writer);
-//                String json = writer.toString();
-//                System.out.print(json);
-//            }
-//        }
-
+        System.out.println(end - start + "ms");*/
     }
 }
