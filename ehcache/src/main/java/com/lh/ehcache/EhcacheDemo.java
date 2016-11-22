f96901b801b445d5d0ec1629fda61578a1df668a
lh <beyondlh@126.com>
2016-11-22 18:34:25 +0800

    添加ehcache初步使用demo

---
+++ ehcache/src/main/java/com/lh/ehcache/EhcacheDemo.java
@@ -0,0 +1,24 @@
+package com.lh.ehcache;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
+
+/**
+ * Created by lh on 2016/11/22.
+ */
+public class EhcacheDemo {
+    public static void main(String[] args) {
+        final CacheManager cacheManager = new CacheManager();
+
+        final Cache cache = cacheManager.getCache("helloworld");
+        final String key = "greeting";
+        final Element putGreeting = new Element(key,"helloworld");
+
+        cache.put(putGreeting);
+        // Retrieve the data element
+        final Element getGreeting = (Element) cache.get(key);// Print the value
+        System.out.println(getGreeting.getObjectValue());
+    }
+}
